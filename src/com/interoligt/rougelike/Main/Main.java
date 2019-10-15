85b71f3cf193b002057287e90a132a317cbcb1b3
Karl Gustafsson <karl.gustafsson@live.com>
2019-10-15 16:04:40 +0200

    JUnit

---
+++ src/com/interoligt/rougelike/Main/Main.java
@@ -0,0 +1,10 @@
+package com.interoligt.rougelike.Main;
+
+public class Main {
+
+
+
+        public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/com/interoligt/rougelike/Main/TestbarKlass.java
@@ -0,0 +1,16 @@
+package com.interoligt.rougelike.Main;
+
+public class TestbarKlass {
+
+    public static boolean testMe(){
+        return true;
+    }
+
+    public boolean instanceMe(){
+        return true;
+    }
+
+    protected boolean imProtected(){
+        return true;
+    }
+}
