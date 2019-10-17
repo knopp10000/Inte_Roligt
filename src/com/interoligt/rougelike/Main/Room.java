b0b51a24558d6d4264cd957d2d953b76f9dc5d6d
Your Name <you@example.com>
2019-10-17 14:36:25 +0200

    added room class

---
+++ src/com/interoligt/rougelike/Main/Room.java
@@ -0,0 +1,32 @@
+package com.interoligt.rougelike.Main;
+
+import java.util.Collection;
+import java.util.List;
+
+
+public class Room {
+
+    private boolean activated = false;
+    private List<Monster> monsters;
+
+    public Room(Monster... m) {
+        this.monsters = new List<Monster>(m);
+    }
+
+    public void activate(){
+        activated = true;
+    }
+
+    public void deActivate(){
+        activated = false;
+    }
+
+    public boolean isActivated(){
+        return activated;
+    }
+
+    public boolean removeMonsterFromRoom(Monster monster){
+        return monsters.remove(monster);
+    }
+
+}
