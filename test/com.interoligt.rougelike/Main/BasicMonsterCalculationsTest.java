f340fbc25cb3306e9e5dd9b561b52b1022979355
linneapa <linneapa@gmail.com>
2019-10-17 14:32:08 +0200

    Added new test class, for calculated values and changeable variables

---
+++ test/com.interoligt.rougelike/Main/BasicMonsterCalculationsTest.java
@@ -0,0 +1,15 @@
+package com.interoligt.rougelike.Main;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class BasicMonsterCalculationsTest {
+    BasicMonster monster;
+
+    @Test
+    void getExperienceRewardTest(){
+        monster = new BasicMonster("Bat", 25, Element.GROUND,1500, 30,25,5);
+        assertEquals(monster.getExperienceReward(),300);
+    }
+
+}
