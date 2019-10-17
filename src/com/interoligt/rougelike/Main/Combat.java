d80a69d19f9094f206de6cbd70e3f4f1d295a00b
mpx9411 <56627018+mpx9411@users.noreply.github.com>
2019-10-17 11:05:19 +0200

    Removed combat from master

--- src/com/interoligt/rougelike/Main/Combat.java
+++ src/com/interoligt/rougelike/Main/Combat.java
@@ -2,11 +2,16 @@ package com.interoligt.rougelike.Main;
 
 public class Combat {
 
-    Monster[] enemies;
-    Room room;
+    private Monster[] enemies;
+    private Room room;
+
     public Combat(Monster[] enemies, Room room){
         this.enemies = enemies;
         this.room = room;
     }
 
+    private void runTurn(){
+        for(Monster i : enemies);
+            
+    }
 }
