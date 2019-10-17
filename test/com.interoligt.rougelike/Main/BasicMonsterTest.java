1485aa0b37c4d6b0682816c0674eea5c41ba311e
linneapa <linneapa@gmail.com>
2019-10-17 12:12:44 +0200

    toString test in test class, constructor in Monster

--- test/com.interoligt.rougelike/Main/BasicMonsterTest.java
+++ test/com.interoligt.rougelike/Main/BasicMonsterTest.java
@@ -1,6 +1,15 @@
 package com.interoligt.rougelike.Main;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BasicMonsterTest {
+class BasicMonsterTest {
+
+    @Test
+    void toStringTest(){
+        BasicMonster monster = new BasicMonster("Spider", 5, Element.FIRE,50, 8, 4, 2);
+        assertEquals(monster.toString(),"Name: Spider, Level: 5, Element: Fire");
+    }
+
 
 }
