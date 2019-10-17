443737c77ce708398d9beea93d2b255adb3c2613
linneapa <linneapa@gmail.com>
2019-10-17 12:12:44 +0200

    toString test in test class, constructor in Monster

--- src/com/interoligt/rougelike/Main/Monster.java
+++ src/com/interoligt/rougelike/Main/Monster.java
@@ -1,10 +1,18 @@
 package com.interoligt.rougelike.Main;
 
 public abstract class Monster {
-    int baseHealth, baseArmour, baseDamage, level, speed;
+    int baseHealth, baseArmour, baseDamage, level, baseSpeed;
     String name;
     boolean isActive, isAlive;
     Element element;
 
-
+    protected Monster(String name, int level, Element element, int baseHealth, int baseArmour, int baseDamage, int baseSpeed){
+        this.name = name;
+        this.element = element;
+        this.baseArmour = baseArmour;
+        this.baseHealth = baseHealth;
+        this.baseDamage = baseDamage;
+        this.baseSpeed = baseSpeed;
+        this.level = level;
+    }
 }
