008566cbc22740db42d5bb495f80e245dc2813e1
mpx9411 <56627018+mpx9411@users.noreply.github.com>
2019-10-17 10:48:05 +0200

    Added combat class

---
+++ src/com/interoligt/rougelike/Main/Combat.java
@@ -0,0 +1,12 @@
+package com.interoligt.rougelike.Main;
+
+public class Combat {
+
+    Monster[] enemies;
+    Room room;
+    public Combat(Monster[] enemies, Room room){
+        this.enemies = enemies;
+        this.room = room;
+    }
+
+}
