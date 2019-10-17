25d9e1bdbb0a81b246dbe62fc905a645592fd817
linneapa <linneapa@gmail.com>
2019-10-17 13:21:03 +0200

    toString test in test class working, get-tests added

--- src/com/interoligt/rougelike/Main/BasicMonster.java
+++ src/com/interoligt/rougelike/Main/BasicMonster.java
@@ -1,4 +1,13 @@
 package com.interoligt.rougelike.Main;
 
 public class BasicMonster extends Monster {
+
+    public BasicMonster(String name, int level, Element element, int baseHealth, int baseArmour, int baseDamage, int baseSpeed){
+        super(name,level,element, baseHealth, baseArmour, baseDamage, baseSpeed);
+;    }
+
+    @Override
+    public String toString(){
+        return "Name: " + super.getName() + ", Level: " + super.getLevel() + ", Element: " + super.getElement().toString().substring(0,1) + super.getElement().toString().substring(1).toLowerCase();
+    }
 }
--- test/com.interoligt.rougelike/Main/BasicMonsterTest.java
+++ test/com.interoligt.rougelike/Main/BasicMonsterTest.java
@@ -4,12 +4,44 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BasicMonsterTest {
+    BasicMonster monster = new BasicMonster("Spider", 5, Element.FIRE, 50, 8, 4, 2);
 
     @Test
     void toStringTest(){
-        BasicMonster monster = new BasicMonster("Spider", 5, Element.FIRE,50, 8, 4, 2);
         assertEquals(monster.toString(),"Name: Spider, Level: 5, Element: Fire");
     }
 
+    @Test
+    void getNameTest(){
+        assertEquals(monster.getName(),"Spider");
+    }
 
+    @Test
+    void getLevelTest(){
+        assertEquals(monster.getLevel(),5);
+    }
+
+    @Test
+    void getElement(){
+        assertEquals(monster.getElement(),Element.FIRE);
+    }
+
+    @Test
+    void getBaseHealth(){
+        assertEquals(monster.getBaseHealth(), 50);
+    }
+
+    @Test
+    void getBaseArmour(){
+        assertEquals(monster.getBaseArmour(),8);
+    }
+
+    @Test
+    void getBaseDamage(){
+        assertEquals(monster.getBaseDamage(),4);
+    }
+    
+    void getBaseSpeed(){
+        assertEquals(monster.getBaseSpeed(),2);
+    }
 }
