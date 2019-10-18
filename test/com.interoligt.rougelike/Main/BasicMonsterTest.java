3b9d0597774c065ca74c576257484822131591ef
linneapa <linneapa@gmail.com>
2019-10-18 21:45:21 +0200

    Added tests for methods setAlive and setActive

--- test/com.interoligt.rougelike/Main/BasicMonsterTest.java
+++ test/com.interoligt.rougelike/Main/BasicMonsterTest.java
@@ -4,20 +4,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BasicMonsterTest {
-
+BasicMonster monster;
 
     @Test
     void isAliveTest(){
+        monster = new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 8, 4, 2);
         assertTrue(monster.isAlive());
     }
 
     @Test
     void isActiveTest(){
+        monster = new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 8, 4, 2);
         assertTrue(monster.isActive());
     }
 
     @Test
     void setActiveTest(){
+        monster = new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 8, 4, 2);
         monster.setActive(false);
         assertFalse(monster.isActive());
         monster.setActive(true);
@@ -26,6 +29,7 @@ class BasicMonsterTest {
 
     @Test
     void setAliveTest(){
+        monster = new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 8, 4, 2);
         monster.setAlive(false);
         assertFalse(monster.isAlive());
         monster.setAlive(true);
