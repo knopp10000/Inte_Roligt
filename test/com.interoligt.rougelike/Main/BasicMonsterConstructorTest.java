bbac88e421eabfac09726860cb3be8e3c2aa78ec
linneapa <linneapa@gmail.com>
2019-10-18 22:19:16 +0200

    Added constructor exception tests

--- test/com.interoligt.rougelike/Main/BasicMonsterConstructorTest.java
+++ test/com.interoligt.rougelike/Main/BasicMonsterConstructorTest.java
@@ -2,10 +2,110 @@ package com.interoligt.rougelike.Main;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BasicMonsterConstructorTest {
     BasicMonster monster = new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 8, 4, 2);
+
+    @Test
+    void constructorNameNullTest() {
+        assertThrows(NullPointerException.class, () -> {
+            new BasicMonster(null, 5, 12, Element.FIRE, 50, 8, 4, 2);
+        });
+    }
+
+    @Test
+    void constructorNameEmptyTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("", 5, 12, Element.FIRE, 50, 8, 4, 2);
+        });
+    }
+
+    @Test
+    void constructorNameSpacesTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("   ", 5, 12, Element.FIRE, 50, 8, 4, 2);
+        });
+    }
+
+    @Test
+    void constructorLevel0Test() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 0, 12, Element.FIRE, 50, 8, 4, 2);
+        });
+    }
+
+    @Test
+    void constructorLevelNegativeTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", -5, 12, Element.FIRE, 50, 8, 4, 2);
+        });
+    }
+
+    @Test
+    void constructorExpPL0Test() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 5, 0, Element.FIRE, 50, 8, 4, 2);
+        });
+    }
+
+    @Test
+    void constructorExpPLNegativeTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 5, -12, Element.FIRE, 50, 8, 4, 2);
+        });
+    }
+    @Test
+    void constructorElementNullTest() {
+        assertThrows(NullPointerException.class, () -> {
+            new BasicMonster("Spider", 5, 12,null, 50, 8, 4, 2);
+        });
+    }
+    @Test
+    void constructorBaseHealth0Test() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 5, 12, Element.FIRE, 0, 8, 4, 2);
+        });
+    }
+
+    @Test
+    void constructorBaseHealthNegativeTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 5, 12, Element.FIRE, -50, 8, 4, 2);
+        });
+    }
+
+    @Test
+    void constructorBaseArmour0Test() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 0, 4, 2);
+        });
+    }
+
+    @Test
+    void constructorBaseDamage0Test() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 8, 0, 2);
+        });
+    }
+
+    @Test
+    void constructorBaseDamageNegativeTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 8, -4, 2);
+        });
+    }
+
+    @Test
+    void constructorBaseSpeed0Test() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 8, 4, 0);});
+    }
+    @Test
+    void constructorBaseSpeedNegativeTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BasicMonster("Spider", 5, 12, Element.FIRE, 50, 8, 4, -2);});
+    }
 
     @Test
     void toStringTest(){
