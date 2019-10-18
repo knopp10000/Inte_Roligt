9b743a032a81cc2c545a995298a559cb1d22ecd8
linneapa <linneapa@gmail.com>
2019-10-18 22:37:11 +0200

    Code added to pass the constructor tests, one test was added

--- src/com/interoligt/rougelike/Main/BasicMonster.java
+++ src/com/interoligt/rougelike/Main/BasicMonster.java
@@ -3,8 +3,11 @@ package com.interoligt.rougelike.Main;
 public class BasicMonster extends Monster {
     private int expPerLevel;
 
-    public BasicMonster(String name, int level, int expPerLevel, Element element, int baseHealth, int baseArmour, int baseDamage, int baseSpeed){
+    public BasicMonster(String name, int level, int expPerLevel, Element element, int baseHealth, int baseArmour, int baseDamage, int baseSpeed) throws IllegalArgumentException{
         super(name,level,element, baseHealth, baseArmour, baseDamage, baseSpeed);
+        if(expPerLevel <= 0){
+            throw new IllegalArgumentException("All integers need to be greater than 0");
+        }
         this.expPerLevel = expPerLevel;
 ;    }
 
