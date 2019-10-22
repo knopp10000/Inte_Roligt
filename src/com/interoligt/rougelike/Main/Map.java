21555e8f814303f8a7033b3b11585c73e4de2375
Karl Gustafsson <karl.gustafsson@live.com>
2019-10-22 12:38:12 +0200

    added Map and mapTest

---
+++ src/com/interoligt/rougelike/Main/Map.java
@@ -0,0 +1,14 @@
+package com.interoligt.rougelike.Main;
+
+public class Map {
+
+    Room[][] rooms;
+
+    public Map(){
+
+    }
+
+    Room move(Character c){
+        return new Room();
+    }
+}
