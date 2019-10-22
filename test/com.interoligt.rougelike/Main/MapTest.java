3d4678711a487f2f161c2df1049c3f5ff9ebdc89
Karl Gustafsson <karl.gustafsson@live.com>
2019-10-22 15:22:30 +0200

    added some tests

--- test/com.interoligt.rougelike/Main/MapTest.java
+++ test/com.interoligt.rougelike/Main/MapTest.java
@@ -1,7 +1,19 @@
 package com.interoligt.rougelike.Main;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapTest {
 
+    Monster anka = new BasicMonster("Anka", 3, 1, Element.FIRE, 4, 2, 1, 200);
+    Room room = new Room(anka);
+    Room room2 = new Room(anka);
+    Room[][] roomArray = {{room, room}, null};
+    Map map = new Map(roomArray);
+
+    @Test
+    void testmoving(){
+        assertTrue(map.move('u') == room2);
+    }
 }
