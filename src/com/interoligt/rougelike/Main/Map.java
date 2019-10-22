bde0c1288cb87e3e9473d10df5d160f2bfd8fc85
Karl Gustafsson <karl.gustafsson@live.com>
2019-10-22 15:22:30 +0200

    added some tests

--- src/com/interoligt/rougelike/Main/Map.java
+++ src/com/interoligt/rougelike/Main/Map.java
@@ -2,13 +2,30 @@ package com.interoligt.rougelike.Main;
 
 public class Map {
 
-    Room[][] rooms;
+    int playerCordX = 0;
+    int playerCordY = 0;
 
-    public Map(){
+    Room[][] map;
 
+    public Map(Room[][] map){
+        this.map = map;
     }
 
-    Room move(Character c){
-        return new Room();
+    Room move(Character dir) throws IllegalArgumentException {
+        switch (dir){
+            case 'u':
+                Room room = map[playerCordX][playerCordY + 1];
+                if (room != null){
+                    playerCordY += 1;
+                }
+                return room;
+            case 'd':
+                return map[playerCordX][playerCordY - 1];
+            case 'r':
+                return map[playerCordX + 1][playerCordY];
+            case 'l':
+                return map[playerCordX - 1][playerCordY];
+        }
+        throw new IllegalArgumentException("only: u, d, r, l are valid inputs.");
     }
 }
