0fea2ca4ecc913a16bd977b2140cf87be0c61ce6
linneapa <linneapa@gmail.com>
2019-10-22 15:34:01 +0200

    Added another constructor test, some tests successful

--- src/com/interoligt/rougelike/Main/Effect.java
+++ src/com/interoligt/rougelike/Main/Effect.java
@@ -1,7 +1,24 @@
 package com.interoligt.rougelike.Main;
 
 public class Effect {
-    int counter;
-    boolean hasDuration;
-    String name;
+   private int duration;
+   private int counter = duration;
+   private boolean hasDuration, continuous;
+   private String name;
+   private double value;
+   private char stat, operator;
+
+    Effect(String name, char stat, char operator, boolean continuous, double value, int duration){
+        this(name, stat,operator,continuous,value);
+        this.duration = duration;
+        hasDuration = true;
+    }
+    Effect (String name, char stat, char operator, boolean continuous, double value){
+        this.name = name;
+        this.stat = stat;
+        this.operator = operator;
+        this.continuous = continuous;
+        this.value = value;
+    }
+
 }
--- test/com.interoligt.rougelike/Main/EffectTest.java
+++ test/com.interoligt.rougelike/Main/EffectTest.java
@@ -7,27 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
 public class EffectTest {
     @Test
     void effectConstructorTest(){
-        Effect armour = new Effect("Armour Increase",'a', '+', 5, 5);
+        Effect armour = new Effect("Armour Increase",'a', '+', false, 5, 5);
         assertEquals(armour.toString(),"Name: Armour Increase, Stat: Armour, Effect: Add 5, Duration: 5 turns");
     }
     @Test
+    void effectConstructorContTest(){
+        Effect health = new Effect("Health Regen",'h', '+', true, 5, 5);
+        assertEquals(health.toString(),"Name: Health Regen, Stat: Health, Effect: Add 5 each turn, Duration: 5 turns");
+    }
+    @Test
     void effectHasDurationTrueTest(){
-        Effect speed = new Effect("Speed",'s','*','o',1.5,3);
+        Effect speed = new Effect("Speed",'s','*',false,1.5,3);
         assertTrue(speed.hasDuration());
     }
     @Test
     void effectHasDurationFalseTest(){
-        Effect armour = new Effect("Armour",'a','-','o',10);
+        Effect armour = new Effect("Armour",'a','-',false,10);
         assertFalse(armour.hasDuration());
     }
     @Test
     void effectAppliedEachTurnTrue(){
-        Effect health = new Effect("Health Drain", 'h', '-', 'c');
+        Effect health = new Effect("Health Drain", 'h', '-', true, 5);
         assertTrue(health.isContinuous());
     }
     @Test
     void effectAppliedEachTurnFalse(){
-        Effect damage = new Effect("Increase Damage", 'd', '*','o');
+        Effect damage = new Effect("Increase Damage", 'd', '*',false, 1);
         assertFalse(damage.isContinuous());
     }
 }
