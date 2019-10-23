71cce1ce8ec7b1ea3d054457b586b1ea0e43c4a6
mpx9411 <56627018+mpx9411@users.noreply.github.com>
2019-10-23 12:16:58 +0200

    updated combat with Target

--- src/com/interoligt/rougelike/Main/Combat.java
+++ src/com/interoligt/rougelike/Main/Combat.java
@@ -4,23 +4,39 @@ public class Combat {
 
     private Monster[] enemies;
     private Player player;
+    private Target[] turnOrder;
 
     public Combat(Monster[] enemies, Player player){
         this.enemies = enemies;
         this.player = player;
+        turnOrder = new Target[enemies.length+1];
     }
 
-    private void runTurnOrder(){
-        Monster[] turnOrder = enemies;
-        for(Monster i : turnOrder) {
-            for (Monster j : turnOrder) {
+    private void setTargetOrder(){
+        for(int i = 0; i < enemies.length; i++){
+            turnOrder[i] = enemies[i];
+        }
+        turnOrder[enemies.length] = player;
+
+        for(Target i : enemies) {
+            for (Monster j : enemies) {
                 if (j.getSpeed() > i.getSpeed()){
-                    Monster temp = i;
+                    Target temp = i;
                     i = j;
                     j = temp;
                 }
             }
         }
 
+    }
+
+
+
+    public Player getPlayer(){
+        return player;
+    }
+
+    public Monster[] getMonsters(){
+        return enemies;
     }
 }
