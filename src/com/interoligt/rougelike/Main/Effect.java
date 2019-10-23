edf6a510c25144baff70c202acd3e849dc0db108
linneapa <linneapa@gmail.com>
2019-10-23 11:55:53 +0200

    Tests successful

--- src/com/interoligt/rougelike/Main/Effect.java
+++ src/com/interoligt/rougelike/Main/Effect.java
@@ -2,22 +2,23 @@ package com.interoligt.rougelike.Main;
 
 public class Effect {
    private int duration;
-   private int counter = duration;
-   private boolean hasDuration, continuous;
+   private int counter;
+   private boolean hasDuration, isContinuous;
    private String name;
-   private double value;
+   private int value;
    private char stat, operator;
 
-    Effect(String name, char stat, char operator, boolean continuous, double value, int duration){
-        this(name, stat,operator,continuous,value);
+    Effect(String name, char stat, char operator, boolean isContinuous, int value, int duration){
+        this(name, stat,operator,isContinuous,value);
         this.duration = duration;
+        counter = duration;
         hasDuration = true;
     }
-    Effect (String name, char stat, char operator, boolean continuous, double value){
+    Effect (String name, char stat, char operator, boolean isContinuous, int value){
         this.name = name;
         this.stat = stat;
         this.operator = operator;
-        this.continuous = continuous;
+        this.isContinuous = isContinuous;
         this.value = value;
     }
 
@@ -25,14 +26,67 @@ public class Effect {
         return hasDuration;
     }
     boolean isContinuous(){
-        return continuous;
+        return isContinuous;
     }
 
     int getDuration(){
-        return duration;
+        if(hasDuration) {
+            return duration;
+        }
+        else{
+            throw new NullPointerException("Effect has no duration.");
+        }
     }
     double getValue(){
         return value;
+    }
+
+    char getStat(){
+        return stat;
+    }
+    int getCounter(){
+        if(hasDuration){
+            return counter;
+        }
+        else{
+            throw new NullPointerException("Effect has no duration.");
+        }
+    }
+
+    String getStatusName(){
+        switch(stat) {
+            case 'h':
+                return "Health";
+            case 's':
+                return "Speed";
+            case 'd':
+                return "Damage";
+            default:
+                return "No Status";
+        }
+    }
+
+    String getEffectType(){
+            switch(operator) {
+                case '+':
+                    return "Add";
+                case '-':
+                    return "Subtract";
+                default:
+                    return "No Effect";
+            }
+        }
+
+    @Override
+    public String toString(){
+        String str = "Name: " + name + ", Stat: " + getStatusName() + ", Effect: " + getEffectType() + " " + value;
+        if(isContinuous){
+            str += " each turn";
+        }
+        if(hasDuration){
+            str += ", Duration: " + duration + " turns";
+        }
+        return str;
     }
 
 
