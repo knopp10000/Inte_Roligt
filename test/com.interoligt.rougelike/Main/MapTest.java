dcee9ff1cbd91b6ea95dcaebe2d5838f3df04f9c
Karl Gustafsson <karl.gustafsson@live.com>
2019-10-28 14:16:40 +0100

    kinda done

--- test/com.interoligt.rougelike/Main/MapTest.java
+++ test/com.interoligt.rougelike/Main/MapTest.java
@@ -7,13 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class MapTest {
 
     Monster anka = new BasicMonster("Anka", 3, 1, Element.FIRE, 4, 2, 1, 200);
-    Room room = new Room(anka);
-    Room room2 = new Room(anka);
-    Room[][] roomArray = {{room, room}, null};
+    Room room00 = new Room(anka);
+    Room room01 = new Room(anka);
+    Room room10 = new Room(anka);
+    Room room11 = new Room(anka);
+
+    Room[][] roomArray = {{room00, room01}, {room10, room11}};
     Map map = new Map(roomArray);
 
     @Test
-    void testmoving(){
-        assertTrue(map.move('u') == room);
+    void testMoveUp(){
+        assertTrue(map.move('u') == room01);
     }
+
+    @Test
+    void testMoveDown(){
+        assertTrue(map.move('d') == null);
+    }
+
+    @Test
+    void testMoveLeft(){
+        assertTrue(map.move('l') == null);
+    }
+
+    @Test
+    void testMoveRight(){
+        assertTrue(map.move('r') == room10);
+    }
+
 }
